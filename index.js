5b59592a27d3fa1243a3fc65afe7c144ab96efbc
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2023-03-04 13:18:00 +0200

    Update ai server

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 8080
-const stockfish = require("stockfish");
-const engine = stockfish();
-const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
-
-engine.onmessage = function(msg) {
-  console.log(msg);
-};
+const port = 7000
+const jsChessEngine = require('js-chess-engine')
+const { move, status, moves, aiMove } = jsChessEngine  
 
 //engine.postMessage("uci");
 
@@ -20,24 +15,16 @@ app.get('/', (request, response) => {
 
 
 app.post('/', (request, response) => {
-// if chess engine replies
-  engine.onmessage = function(msg) {
-    //console.log(msg);
-    // in case the response has already been sent?
-    if (response.headersSent) {
-        return;
-    }
-    // only send response when it is a recommendation
-    if (typeof(msg == "string") && msg.match("bestmove")) {
-      console.log(msg);
-      response.send(msg);
-    }
+  try{
+    var nextMove = aiMove(request.body.fen);
+    console.log(nextMove);
+    var stringMove = Object.keys(nextMove)[0] + "-" + nextMove[Object.keys(nextMove)[0]];
+    response.send(stringMove);
   }
-
-// run chess engine
-  engine.postMessage("ucinewgame");
-  engine.postMessage("position fen " + request.body.fen);
-  engine.postMessage("go depth 18");
+  catch{
+    response.send("Fail");
+  }
+  
 
 });
 
